1cc9f9a01da9da0c9c3f1d33d548ca25a6124323
ajax5692 <nn.abhra@gmail.com>
2026-05-26 18:50:57 +0200

    + added getFighterDetails method to get detailed fighter info. Modification in src/javascript/services/fightersService.js

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -3,6 +3,8 @@ import callApi from '../helpers/apiHelper';
 class FighterService {
     #endpoint = 'fighters.json';
 
+    #detailsEndpoint = 'details/fighter/'; // 1. Add this private property
+
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
@@ -13,8 +15,19 @@ class FighterService {
     }
 
     async getFighterDetails(id) {
-        // todo: implement this method
-        // endpoint - `details/fighter/${id}.json`;
+        try {
+            // 1. Construct the specific endpoint for this fighter using backticks
+            const endpoint = `${this.#detailsEndpoint}${id}.json`;
+
+            // 2. Call the API helper with this new endpoint
+            const apiResult = await callApi(endpoint);
+
+            // 3. Return the detailed data
+            return apiResult;
+        } catch (error) {
+            // 4. Pass the error up if the network request fails
+            throw error;
+        }
     }
 }
 
